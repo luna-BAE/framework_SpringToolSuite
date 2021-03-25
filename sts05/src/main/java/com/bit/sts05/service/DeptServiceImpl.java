d16e7bef31a60895531ba3793ddaaf30d93917bb
luna-BAE <dichotomy.bgm@gmail.com>
2021-03-25 13:06:34 +0900

    Spring service Test

---
+++ sts05/src/main/java/com/bit/sts05/service/DeptServiceImpl.java
@@ -0,0 +1,55 @@
+package com.bit.sts05.service;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.bit.sts05.model.DeptDao;
+import com.bit.sts05.model.entity.DeptVo;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+	// 오토와이어와 같지만 자바 외부 라이브러리라서 어디서든 쓸 수 있음, 오토와이어는 스프링에서만 쓸 수 있음, 인젝트는 스프링이 아니어도 동작
+	@Inject
+	SqlSession sqlSession;
+	String dao = DeptDao.class.toString();
+	
+	@Override
+	public void selectAll(Model model) {
+		DeptDao deptDao = sqlSession.getMapper(DeptDao.class);
+		model.addAttribute("list", deptDao.selectAll());
+
+	}
+
+	@Override
+	public void selectOne(Model model, int key) {
+		DeptDao deptDao = sqlSession.getMapper(DeptDao.class);
+		model.addAttribute("bean", deptDao.selectOne(key));
+
+	}
+
+	@Override
+	public void insertOne(DeptVo bean) {
+		DeptDao deptDao = sqlSession.getMapper(DeptDao.class);
+		deptDao.insertOne(bean);
+
+	}
+
+	@Override
+	public void updateOne(DeptVo bean) {
+		DeptDao deptDao = sqlSession.getMapper(DeptDao.class);
+		deptDao.updateOne(bean);
+
+	}
+
+	@Override
+	public void deleteOne(int key) {
+		DeptDao deptDao = sqlSession.getMapper(DeptDao.class);
+		deptDao.deleteOne(key);
+
+	}
+
+}
---
+++ sts05/src/test/java/com/bit/sts05/service/DeptServiceTest.java
@@ -0,0 +1,85 @@
+package com.bit.sts05.service;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+import com.bit.sts05.model.entity.DeptVo;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
+// 원래 이게 돼야하는데, servlet-context.xml에 정의되어 있는 스키마때문에 안 되네요.
+// @ContextConfiguration(value = {"classpath:/applicationContext.xml",
+//		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+public class DeptServiceTest {
+
+	@Inject
+	DeptService deptService;
+	DeptVo target;
+	Model model;
+	
+	@Before
+	public void setUp() throws Exception {
+		target = new DeptVo(88, "test", "test");
+		model = new ExtendedModelMap();
+	}
+	
+
+	@Test
+	public void test1SelectAll() {
+		try {
+		deptService.selectAll(model);
+		Map<String, Object> map = model.asMap();
+		assertFalse(map.isEmpty());
+		} catch (Exception e) {
+			fail("not yet");
+		}
+	}
+
+	@Test
+	public void test4SelectOne() {
+		deptService.selectOne(model, target.getDeptno());
+		assertTrue(model.containsAttribute("bean")); // 불린타입으로 빈이 존재하면
+	}
+
+	@Test
+	public void test2InsertOne() {
+		try {
+		deptService.insertOne(target);
+		}catch (Exception e) {
+			fail();
+		}
+	}
+
+	@Test
+	public void test3UpdateOne() {
+		try {
+		deptService.updateOne(target);
+		}catch (Exception e) {
+			fail();
+		}
+	}
+
+	@Test
+	public void test5DeleteOne() {
+		try {
+		deptService.deleteOne(target.getDeptno());
+		}catch (Exception e) {
+			fail();
+		}
+	}
+
+}
